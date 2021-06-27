88181ea5a4a67c333b50b43bc1d37626fb30177e
ZJW <31417311+zijianKING@users.noreply.github.com>
2021-06-26 22:32:39 -0400

    Add files via upload

--- openFile3.py
+++ openFile3.py
@@ -1,13 +1,13 @@
 import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtWidgets import QInputDialog
+from PyQt5.QtWidgets import QInputDialog, QFileDialog, QPushButton
 
 class Window(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(50, 50, 500, 300)
-        self.setWindowTitle("FireSafety")
+        self.setWindowTitle("Fire Safety Guide Generation")
         self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
 
         openFile = QtWidgets.QAction("&Open File", self)
@@ -19,8 +19,13 @@ class Window(QtWidgets.QMainWindow):
 
         mainMenu = self.menuBar()
         
-        fileMenu = mainMenu.addMenu('&File')
+        fileMenu = mainMenu.addMenu('&Open File')
         fileMenu.addAction(openFile)
+
+        pybutton = QPushButton('Open File', self)
+        pybutton.clicked.connect(self.file_open)
+        pybutton.resize(200,32)
+        pybutton.move(80, 60)        
 
         self.home()
 
@@ -29,33 +34,59 @@ class Window(QtWidgets.QMainWindow):
         self.show()
 
     def file_open(self):
+        name = QFileDialog.getOpenFileName(self, 'Open File')
+        file1 = open(name[0],'r')
+        print(file1)
+
+        #self.editor()
+
+        with file1:
+            text = file1.read()
+            print(text)
+            #self.textEdit.setText(text)
+
+        #dlg.setFilter("Text files (*.txt)")
+        #filenames = QStringList()
         #name = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File')
 
-        # Import Sophie's Code 
 
+        # Import Sophie's Code
         # Modeloutput = RunSophie()
+        #SophieResult = ['toxic','hot']
+        SophieResult = ['toxic','hot']
+        
+        # The dictionary for Guide and Keyword mapping
+        Modeloutput = {'toxic':['guide 137.txt','guide 138.txt','guide 139.txt','guide 144.txt'], 'Corrosive':['guide 137.txt'], \
+            'explosive':['guide 137.txt','guide 138.txt'],'hot':['guide 137.txt','guide 138.txt','guide 139.txt','guide 144.txt']}
 
-        Modeloutput = [('toxic',['guide 137.txt','guide 138.txt','guide 139.txt','guide 144.txt']), ('Corrosive',['guide 137.txt']), \
-            ('explosive',['guide 137.txt','guide 138.txt']),('hot',['guide 137.txt','guide 138.txt','guide 139.txt','guide 144.txt'])]
-
-        address = 'C:\\Users\\zijia\\Downloads\\Fire_Safety-openFile\\Fire_Safety-openFile'
+        address = 'C:\\Users\\zijia\\Downloads\\Fire_Safety-openFile\\Fire_Safety-openFile\\'
 
         filenames = []
         
-        for each in Modeloutput:
-            for i in each[1]:
-                filename = address + i
-                filenames.append(filename)
+        for each in SophieResult:
+            for guide in Modeloutput[each]:
+                    filenames.append(address + guide)
+        filenames = list(dict.fromkeys(filenames))
 
-        name, ok = QInputDialog.getText(self, "Open File", "Enter File Name")
-        print(name)
-        file = open(name,'r')
+        #name, ok = QInputDialog.getText(self, "Open File", "Enter File Name")
+        #print(name)
+        data = ""
+		
+		# Reading data from file1
+        for item in filenames:
+            with open(item) as fp:
+                data += fp.read()
+                data += "\n"
+            file = open(item,'r')
 
+		#for i in filenames:
+		#	file = open(i,'r')
+			
         self.editor()
 
         with file:
-            text = file.read()
-            self.textEdit.setText(text)
+            #text = file.read()
+            self.textEdit.setText(data)
 
     def color_picker(self):
         color = QtWidgets.QColorDialog.getColor()
