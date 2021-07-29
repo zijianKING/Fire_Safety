e60984fa88122fd832431b64a33a6236418f963c
ZJW <31417311+zijianKING@users.noreply.github.com>
2021-07-28 23:50:02 -0400

    new change

--- Fire_Safety-cpp_ui/Fire_Safety-cpp_ui/QT_FireSafety/FireSafety/secdialog.cpp
+++ Fire_Safety-cpp_ui/Fire_Safety-cpp_ui/QT_FireSafety/FireSafety/secdialog.cpp
@@ -2,19 +2,37 @@
 #include "ui_mainwindow.h"
 #include "ui_secdialog.h"
 
+#include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QDebug>
 
 SecDialog::SecDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::SecDialog)
 {
-    ui->setupUi(this);
+    QFile file("C:\\Users\\zijia\\Desktop\\Fire_Safety-cpp_ui\\chemicals.txt");
+    QStringList chemicals;
 
-    ui->comboBox->addItem("CO2 ppm 191c");
-    ui->comboBox->addItem("Benzene  191c");
-    ui->comboBox->addItem("Acetylene");
-    ui->comboBox->addItem("CO (500) 191C (1of2)");
-    ui->comboBox->addItem("NO (350,3000) 191C");
-    ui->comboBox->addItem("HF ppm (10) 191C");
+    if( !file.open(QIODevice::ReadOnly) )
+        QMessageBox::information(0,"info",file.errorString());
+
+    QTextStream textStream(&file);
+
+    while( !textStream.atEnd( ) )
+    {
+        QString line = textStream.readLine();
+        ui->comboBox->addItem(line);
+        qDebug() << line;
+        //chemicals.append(line);
+    }
+
+    file.close( );
+
+    //ui->comboBox->addItems(chemicals);
+    //for (int i=0;i < stringList.size(); ++i){
+    //    ui->comboBox->addItem(stringList.at(i));
+    //}
 }
 
 SecDialog::~SecDialog()
@@ -27,7 +45,7 @@ void SecDialog::on_pushButton_clicked()
 {
     ui->label_2->setText("Graph for " + ui->comboBox->currentText());
 
-    QPixmap pm("C:\\Users\\zijia\\Downloads\\Fire_Safety-cpp_ui\\" +ui->comboBox->currentText() + ".png");
+    QPixmap pm("C:\\Users\\zijia\\Desktop\\Fire_Safety-cpp_ui\\" +ui->comboBox->currentText() + " figure.jpg");
     ui->label_3->setPixmap(pm);
     ui->label_3->setScaledContents(true);
     QApplication::restoreOverrideCursor();
